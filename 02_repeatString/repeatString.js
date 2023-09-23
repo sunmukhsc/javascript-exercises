4782948ff9a238a51baf8289c93d1bd0da826f20
Sunmukh <sunmukhsc@gmail.com>
2023-09-22 17:27:08 -0700

    Solve first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnStr = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        returnStr += string;
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returnStr = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        returnStr = returnStr.concat(string.charAt(i));
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
