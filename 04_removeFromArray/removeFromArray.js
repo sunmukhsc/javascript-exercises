ec8c85f662190ebf31d14874ca641b236f6f6b8d
Sunmukh <sunmukhsc@gmail.com>
2023-09-23 00:08:33 -0700

    Solve using nested for loops, passing all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i< args.length; i++) {
+        for (let j = 0; j< array.length; j++) {
+            if (args[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
