f46be63a30623e85c6ee85f9304c761c137bba40
Sunmukh <sunmukhsc@gmail.com>
2023-09-29 13:21:27 -0700

    Design recursive function for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let numConvert = +num;
+    if (numConvert < 1) return "OOPS";
+    if (numConvert === 1 || numConvert === 2) return 1;
 
+    return fibonacci(numConvert - 1) + fibonacci(numConvert - 2);
 };
 
 // Do not edit below this line
