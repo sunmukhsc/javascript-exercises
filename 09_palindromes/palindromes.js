8dbc5d351c1cf58d57cbd5d922c884c840eb8482
Sunmukh <sunmukhsc@gmail.com>
2023-09-28 23:02:52 -0700

    Add function to check palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    stringSimple = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return stringSimple.split('').reverse().join('')===stringSimple ? true : false;
 };
 
 // Do not edit below this line
