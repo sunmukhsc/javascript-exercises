54f10db7d05268f64c74f693e9c9df7fe464ab21
Sunmukh <sunmukhsc@gmail.com>
2023-09-29 14:56:17 -0700

    Return oldest person using reduce() function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (oldest===undefined) {
+            return person;
+        } 
 
+        const currentYear = (new Date()).getFullYear();
+        let oldestAge;
+        let personAge;
+
+        if (!(oldest.hasOwnProperty("yearOfDeath"))) {
+            oldestAge = currentYear - oldest.yearOfBirth;
+        } else {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        if (!(person.hasOwnProperty("yearOfDeath"))) {
+            personAge = currentYear - person.yearOfBirth;
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (personAge > oldestAge) {
+            return person;
+        }else {return oldest;}
+
+    });
 };
 
 // Do not edit below this line
